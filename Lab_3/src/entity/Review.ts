aca3dc6938d5fd3b7fb8190856073bc3c241c94f
Nikki1i <96845712+Nikki1i@users.noreply.github.com>
2021-12-29 23:47:32 +0200

    Create Review.ts

---
+++ Lab_3/src/entity/Review.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from './User.js';
+import { Anime } from './Anime.js';
+
+@Entity({ name: 'review' })
+export class Review {
+
+    @PrimaryGeneratedColumn()
+    review_id: number;
+
+    @Column({ type: 'character varying', length: 1000, nullable: false })
+    r_text: string;
+
+    @Column({ type: 'integer', nullable: false })
+    @ManyToOne(() => User, (user: User) => user.user_id)
+    @JoinColumn({ name: 'rev_user_id' })
+    rev_user_id: number;
+
+    @Column({ type: 'integer', nullable: false })
+    @ManyToOne(() => Anime, (anime: Anime) => anime.anime_id)
+    @JoinColumn({ name: 'rev_anime_id' })
+    rev_anime_id: number;
+
+}
