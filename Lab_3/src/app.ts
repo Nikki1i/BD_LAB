6d40432f9875eea8d60954f1b230add1e0f1b6a0
Nikki1i <96845712+Nikki1i@users.noreply.github.com>
2021-12-29 23:40:28 +0200

    Create app.ts

---
+++ Lab_3/src/app.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+
+import { Anime } from "./entity/Anime.js";
+import { Genre } from "./entity/Genre.js";
+import { Passport } from "./entity/Passport.js";
+import { User } from "./entity/User.js";
+import { Review } from "./entity/Review.js";
+import { Watched } from "./entity/Watched.js";
+import { UserPassport } from "./entity/UserPassport.js";
+
+import { Controller } from "./controller/Controller.js";
+
+createConnection({
+    type: "postgres",
+    host: "127.0.0.1",
+    port: 5432,
+    username: "postgres",
+    password: "qwerty",
+    database: "anime",
+    entities: [ User, Anime, Genre, Passport, Review, Watched, UserPassport ],
+    synchronize: false,
+    logging: false
+}).then(async connection => {
+    Controller.start(connection);
+}).catch(error => console.log(error));
